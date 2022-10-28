64fcde4679b7591aa49f60ea3301515139301909
SpywarePersues <spywarepersues284@gmail.com>
2022-10-28 19:28:37 +0530

    Minor update

---
+++ pages/upload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContainerBlock from '../components/ContainerBlock'
+
+function upload() {
+    return (
+        <ContainerBlock title="Upload">
+            <h1 className='text-center my-20 text-7xl'>IN PROGRESS...</h1>
+        </ContainerBlock>
+    )
+}
+
+export default upload
