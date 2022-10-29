10201e73c752f2a83d1aa5d8a246f0d3b5bec71f
SpywarePersues <spywarepersues284@gmail.com>
2022-10-29 15:46:17 +0530

    Design fixes

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,10 @@ export default function Home() {
     }
     return (
         <ContainerBlock title="Home">
-            <div className='my-10 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
+            <div className='my-10 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3'>
                 {firedata.map((data) => {
                     return(
-                        <a href={`/videos/${data.id}`} key={data.id} className="glassmorph md:mx-16 mx-6 w-[18rem] lg:w-[18rem] xl:w-[21rem] rounded-lg hover:scale-105 transition-scale duration-300 my-6">
+                        <a href={`/videos/${data.id}`} key={data.id} className="glassmorph md:mx-10 mx-4 w-[18rem] lg:w-[18rem] xl:w-[21rem] rounded-lg hover:scale-105 transition-scale duration-300 my-6">
                             <img src={data.thumbnail} className="w-[46rem] rounded-md" alt='' />
                             <h1 className='md:text-3xl text-2xl my-4 text-center font-bold'>{data.name}</h1>
                             <div className='flex lg:w-7/12 md:w-8/12 w-9/12 mx-auto'>
