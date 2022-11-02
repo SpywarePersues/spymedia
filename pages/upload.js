c0a3155cbcccf8e3f55ce7ce8ee1d21b0136193d
SpywarePersues <spywarepersues284@gmail.com>
2022-11-02 21:18:49 +0530

    Minor fix

--- pages/upload.js
+++ pages/upload.js
@@ -46,16 +46,16 @@ function upload() {
             {token ? (<div>
             <h1 className='text-5xl text-center pb-5 pt-5 text-gray-200 font-semibold'>Upload</h1>
             <h1 className='mx-6 text-xl mt-6'>Name</h1>
-            <input className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setName(event.target.value)} />
+            <input required className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setName(event.target.value)} />
 
             <h1 className='mx-6 text-xl mt-6'>Thumbnail URL</h1>
-            <input className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setThumbnail(event.target.value)} />
+            <input required className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setThumbnail(event.target.value)} />
 
             <h1 className='mx-6 text-xl mt-6'>Video URL <span className='text-blue-600 cursor-pointer'><Link href="https://cloudinary.com" target="_blank">(https://cloudinary.com)</Link></span></h1>
-            <input className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setVideo(event.target.value)} />
+            <input required className='mx-6 rounded-md glassmorph my-2 w-11/12 h-12 p-4 text-gray-300 outline-none' onChange={event => setVideo(event.target.value)} />
 
             <h1 className='mx-6 text-xl mt-6'>Description</h1>
-            <textarea className='mx-6 rounded-md glassmorph mt-2 mb-6 w-11/12 h-[10rem] p-4 text-gray-300 outline-none' onChange={event => setDesc(event.target.value)} />
+            <textarea required className='mx-6 rounded-md glassmorph mt-2 mb-6 w-11/12 h-[10rem] p-4 text-gray-300 outline-none' onChange={event => setDesc(event.target.value)} />
 
             <button className="button px-4 py-3 rounded-md mb-4 mx-6 block" onClick={addData} >Upload</button>
             </div>) : (<div className='text-7xl text-center my-10 underline text-blue-600'><Link href={{pathname: '/login'}}>Login first to upload videos.</Link></div>)}
